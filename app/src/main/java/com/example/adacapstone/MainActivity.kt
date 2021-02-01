cc183699464fc1a0f1a7a9ec5b6edea88a8f9385
Anna Kim <anna.kim93@gmail.com>
2021-02-01 13:03:41 -0500

    set nav item selection listener

--- app/src/main/java/com/example/adacapstone/MainActivity.kt
+++ app/src/main/java/com/example/adacapstone/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.adacapstone
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,10 +12,38 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textMessage: TextView
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                textMessage.text = "Home"
+//                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add -> {
+                textMessage.text = "Add Post"
+//                item.isChecked = false
+//                startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_contacts -> {
+                textMessage.text = "Contacts"
+//                moveToFragment(ContactsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
 //        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
