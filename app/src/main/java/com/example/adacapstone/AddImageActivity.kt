e8c1a770ff65b5512e8a4e542f032cd8db7fc1a6
Anna Kim <anna.kim93@gmail.com>
2021-02-02 01:27:16 -0500

    new activity to add image

---
+++ app/src/main/java/com/example/adacapstone/AddImageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.adacapstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_image)
+    }
+}
