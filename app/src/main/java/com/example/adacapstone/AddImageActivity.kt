8c8925ad048729eb267e6aa1ea86e3e5ac55c2d8
Anna Kim <anna.kim93@gmail.com>
2021-02-02 04:42:14 -0500

    complete tab listener to open up camera on click

--- app/src/main/java/com/example/adacapstone/AddImageActivity.kt
+++ app/src/main/java/com/example/adacapstone/AddImageActivity.kt
@@ -1,13 +1,16 @@
 package com.example.adacapstone
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.core.app.ActivityCompat
 import com.google.android.material.tabs.TabLayout
 
 class AddImageActivity : AppCompatActivity() {
+    private val CAMERA_TAB_NUM = 1
     private val VERIFY_PERMISSIONS_REQUEST_CODE = 1
     private val CAMERA_REQUEST_CODE = 5
 
@@ -22,14 +25,29 @@ class AddImageActivity : AppCompatActivity() {
         }
 
         val tabLayout: TabLayout = findViewById(R.id.bottom_tabs)
-//        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-//            override fun onTabSelected(tab: TabLayout.Tab?) {
-//                val tabNum = tab?.position
-//                if (tabNum == 1) {
-//                    if (this@AddImageActivity.)
-//                }
-//            }
-//        })
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val tabNum = tab?.position
+                if (tabNum == CAMERA_TAB_NUM) {
+                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+                }
+            }
+
+            override fun onTabUnselected (tab: TabLayout.Tab) {
+            }
+
+            override fun onTabReselected (tab: TabLayout.Tab) {
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+
+        }
     }
 
     // Permissions handling
--- app/src/main/java/com/example/adacapstone/Permissions.kt
+++ app/src/main/java/com/example/adacapstone/Permissions.kt
@@ -9,10 +9,10 @@ class Permissions {
           Manifest.permission.READ_EXTERNAL_STORAGE
         )
 
-        val CAMERA_PERMISSION: Array<String> = arrayOf(Manifest.permission.CAMERA)
+        val CAMERA_PERMISSION = Manifest.permission.CAMERA
 
-        val WRITE_STORAGE_PERMISSION: Array<String> = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val WRITE_STORAGE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE
 
-        val READ_STORAGE_PERMISSION: Array<String> = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        val READ_STORAGE_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
     }
 }
