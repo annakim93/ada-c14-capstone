de4b5ec086740c7eeac85599386643927e308880
Anna Kim <anna.kim93@gmail.com>
2021-02-05 12:03:27 -0500

    set up navgraph and add parcelize to gradle build config (app)

--- app/src/main/java/com/example/adacapstone/data/model/ImageMessage.kt
+++ app/src/main/java/com/example/adacapstone/data/model/ImageMessage.kt
@@ -1,14 +1,17 @@
 package com.example.adacapstone.data.model
 
 import android.graphics.Bitmap
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "img_msg_table")
 data class ImageMessage(
     val msg: String,
     val image: Bitmap
-    ) {
+    ) : Parcelable {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
