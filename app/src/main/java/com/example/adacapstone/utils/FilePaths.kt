81ee3ee155426295fe6c6851f37693f0b65db4df
Anna Kim <anna.kim93@gmail.com>
2021-02-05 01:31:50 -0500

    remove unused classes; set up update fragment layout

--- app/src/main/java/com/example/adacapstone/utils/FilePaths.kt
+++
@@ -1,9 +0,0 @@
-package com.example.adacapstone.utils
-
-import android.content.Context
-
-class FilePaths(context: Context) {
-    var ROOT_DIR: String = "/storage/emulated/0"
-    var PICS_DIR: String = "$ROOT_DIR/Pictures"
-    var CAM_DIR = "$ROOT_DIR/DCIM/camera"
-}
