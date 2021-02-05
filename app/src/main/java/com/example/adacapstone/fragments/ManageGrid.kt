fd20df9f9bdb7edb961d7c2ec57e32d5563a392b
Anna Kim <anna.kim93@gmail.com>
2021-02-05 01:15:03 -0500

    setting up 3x3 grid view

--- app/src/main/java/com/example/adacapstone/fragments/ManageGrid.kt
+++ app/src/main/java/com/example/adacapstone/fragments/ManageGrid.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.adacapstone.R
+import com.example.adacapstone.data.viewmodel.ImgMsgViewModel
 import com.example.adacapstone.utils.ListImageAdapter
 
 class ManageGrid : Fragment() {
+
+    private val NUM_GRID_COLUMNS = 3
+    private lateinit var mImgMsgViewModel: ImgMsgViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +30,17 @@ class ManageGrid : Fragment() {
         val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_home)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // ImgMsgModel
+        mImgMsgViewModel = ViewModelProvider(this).get(ImgMsgViewModel::class.java)
+        mImgMsgViewModel.readAllData.observe(viewLifecycleOwner, Observer { imgMsg ->
+            adapter.setData(imgMsg)
+        })
+
+        // Grid setup
+        val gridWidth = resources.displayMetrics.widthPixels
+        val imgWidth = gridWidth / NUM_GRID_COLUMNS
+        
 
         return view
     }
