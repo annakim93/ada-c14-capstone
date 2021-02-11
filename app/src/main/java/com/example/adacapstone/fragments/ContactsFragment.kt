a2bd7d3380bb22d75d23f56720653f38acde052f
Anna Kim <anna.kim93@gmail.com>
2021-02-11 12:56:34 -0500

    add back button click listener to nav back to home fragment

--- app/src/main/java/com/example/adacapstone/fragments/ContactsFragment.kt
+++ app/src/main/java/com/example/adacapstone/fragments/ContactsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.adacapstone.R
@@ -12,12 +13,26 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContactsFragment : Fragment() {
 
+    private lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Click listener on close btn to navigate back to home fragment
+        navController = Navigation.findNavController(view)
+        val backBtn: ImageView = view.findViewById(R.id.back_btn)
+
+        backBtn.setOnClickListener {
+            navController.navigate(R.id.action_contactsFragment_to_homeFragment)
+        }
     }
 
 }
