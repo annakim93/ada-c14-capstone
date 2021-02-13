d02315a61b92154b1c715a344243eb7e61f19754
Anna Kim <anna.kim93@gmail.com>
2021-02-13 18:16:21 -0500

    update nav graph to include addtl contact list update fragment

---
+++ app/src/main/java/com/example/adacapstone/fragments/UpdateSetContactsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.adacapstone.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.adacapstone.R
+
+class UpdateSetContactsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update_set_contacts, container, false)
+
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
